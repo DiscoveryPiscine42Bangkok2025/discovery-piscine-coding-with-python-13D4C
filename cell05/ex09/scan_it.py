8d474470081b61e6ecac41938e8eb86ded2bf6ee
D4C <115446911+13D4C@users.noreply.github.com>
2025-09-15 02:37:15 +0700

    uwu

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -3,7 +3,7 @@ import re
 if len(sys.argv) != 3:
     print("none")
 else:
-    keyword = sys.argv[1]a
+    keyword = sys.argv[1]
     string_to_search = sys.argv[2]
     found_matches = re.findall(keyword, string_to_search)
     if not found_matches:
